dc6fba98274193c22faf7a6d386a3a0d2b6995f1
Capitaine-Vole <capitainevole@gmail.com>
2022-06-02 18:05:53 +0200

    rm of cx_freeze.py

--- cx_freeze.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-# On appelle la fonction setup
-setup(
-    name = "kg.py",
-    version = "1",
-    description = "je ne dirais rien O_o",
-    executables = [Executable("thekg.py")],
-)
